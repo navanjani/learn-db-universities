d4cb1c457fb7e5c4b2d92bdc3cb12c59a40595d2
navanjani <navanjani@outlook.com>
2022-07-12 21:29:41 +0200

    Model generate

---
+++ migrations/20220712185220-create-university.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('universities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      location: {
+        type: Sequelize.STRING
+      },
+      rank: {
+        type: Sequelize.INTEGER
+      },
+      founded_in: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('universities');
+  }
+};
---
+++ models/university.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class university extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  university.init({
+    name: DataTypes.STRING,
+    location: DataTypes.STRING,
+    rank: DataTypes.INTEGER,
+    founded_in: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'university',
+  });
+  return university;
+};
